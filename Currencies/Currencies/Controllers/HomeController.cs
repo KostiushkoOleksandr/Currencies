1beb339bab95fb7b83c7f9e2001b03043ee276ba
KostiushkoOleksandr <oleksandr.kostiushko@oa.edu.ua>
2022-05-17 23:30:57 +0300

    Currencies
    Currencies

---
+++ Currencies/Currencies/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Currencies.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Currencies.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IWebHostEnvironment Environment;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment environment)
+        {
+            _logger = logger;
+            Environment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(string.Concat(this.Environment.WebRootPath, "/Employee.xml"));
+
+            foreach (XmlNode node in doc.SelectNodes("./Employees/Employee"))
+            {
+                employees.Add(new EmployeeModel
+                {
+                    CurrencyCode = int.Parse(node["CurrencyCode"].InnerText),
+                    CurrencyCodeL = node["CurrencyCodeL"].InnerText,
+                    Amount = node["Amount"].InnerText
+
+                });
+            }
+            return View(employees);
+        }
+    }
+}
---
+++ Currencies/Currencies/Models/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Currencies.Models
+{
+    public class EmployeeModel
+    {
+        public int CurrencyCode { get; set; }
+
+        public string CurrencyCodeL { get; set; }
+
+        public string Amount { get; set; }
+    }
+}
